940ac2d5af3eeb84d9672367001e2c3e05ff8818
Dominik Kozaczko <dominik@kozaczko.info>
2026-06-05 11:05:23 +0200

    chore: bump all packages to 0.2.0

--- src/sendparcel_cli/__init__.py
+++ src/sendparcel_cli/__init__.py
@@ -1,6 +1,6 @@
 """Developer CLI for python-sendparcel provider testing and debugging."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from sendparcel_cli.config import ConfigManager
 from sendparcel_cli.models import CLIShipment, CLIShipmentRepository
